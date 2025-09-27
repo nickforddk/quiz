9fb9d91ff7470db6dd0069bdffc130b8d86b8b6c
nickforddk <git@nickford.dk>
2025-09-27 20:11:25 +0200

    Configure Pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const base = process.env.NODE_ENV === 'production' ? '/quiz/' : '/';
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  base,
-  plugins: [react()],
+  base: '/',        // custom domain => root
+  plugins: [react()]
 });
