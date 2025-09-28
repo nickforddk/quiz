62cfcbcbbea6ddf9405a9516b58bddc59c51ed20
nickforddk <git@nickford.dk>
2025-09-28 07:50:06 +0200

    Screen view updates: QR code and scoreboard style

--- src/routes/PublicScreen.tsx
+++ src/routes/PublicScreen.tsx
@@ -18,7 +18,14 @@ export default function PublicScreen() {
     return () => { unsubState(); unsubAnswers(); };
   }, []);
 
-  if (!state || !quiz) return <div className="p-8 text-2xl">Waiting for quiz...</div>;
+  if (!state || !quiz) return (
+    <div className="space-y-8 h-screen fullscreen">
+      <div className="p-8 text-2xl h-full flex flex-col gap-4 justify-center items-center">
+        <span className="qrcode size-50"></span>
+        git.nickford.com/quiz
+      </div>
+    </div>
+  );
   const q = quiz.questions[state.currentQuestion];
   if (!q) return <div className="p-8">No question.</div>;
 
@@ -36,11 +43,14 @@ export default function PublicScreen() {
   const showScoreboard = !!state.scoreboardVisible; // NEW
 
   return (
-    <div className="space-y-8 h-screen fullscreen">
-      <div className="space-y-4 h-full flex flex-col justify-between">
-        <div className="flex flex-row justify-between text-sm font-semibold text-gray-500"><span>Q{state.currentQuestion+1} / {quiz.questions.length}</span>{quiz.name}</div>
-        <h2 className="font-bold mt-auto">{q.text}</h2>
-        <div className="grid gap-4 md:grid-cols-2 auto-rows-fr mb-auto">
+    <>
+    <div className="h-screen fullscreen flex flex-col md:flex-row md:gap-8">
+      <div className="space-y-4 h-full flex flex-col md:justify-center overflow-auto">
+        <div className="flex flex-row justify-between text-sm font-semibold text-gray-500">
+          <span>Q {state.currentQuestion+1} / {quiz.questions.length}</span>
+        </div>
+        <h2 className="font-bold mb-8">{q.text}</h2>
+        <div className="grid gap-4 md:grid-cols-2 auto-rows-fr">
           {q.options.map((o:string) => {
             const count = counts[o];
             const correct = state.reveal && q.correct === o;
@@ -48,7 +58,7 @@ export default function PublicScreen() {
               <div key={o} className={[
                 'p-4 pr-12 rounded border relative',
                 !correct && state.reveal && 'text-grey-500',
-                correct && 'border-green-700 bg-green-500 text-white'
+                correct && 'border-green-600 dark:border-green-300 bg-green-500 text-white'
               ].filter(Boolean).join(' ')}>
                 <div className="font-semibold min-h-8 text-lg">{o}</div>
                 <div className="text-3xl font-bold absolute top-4 right-4 leading-none tabular-nums">
@@ -57,29 +67,37 @@ export default function PublicScreen() {
               </div>
             );
           })}
+        </div>          
+      </div>
+      <div className="bg-grey-500 dark:bg-blue-700 text-white flex md:flex-col md:w-50 justify-between text-sm">
+        <div className="flex flex-col p-4 gap-1">
+          <span className="font-semibold">{quiz.name}</span>
+          {state.ended && 'Quiz ended'}
+          {!state.ended && state.open && 'Accepting answers'}
+          {!state.ended && !state.open && !state.reveal && 'Round closed'}
+          {!state.ended && state.reveal && 'Answers revealed'}
         </div>
-        <div className="text-sm font-semibold text-gray-500">
-          {state.ended && 'Quiz ended.'}
-          {!state.ended && state.open && 'Accepting answers...'}
-          {!state.ended && !state.open && !state.reveal && 'Round closed.'}
-          {!state.ended && state.reveal && 'Answers revealed.'}
+        <div className="bg-grey-400 text-blue-800 dark:bg-grey-800 dark:text-grey-400 p-4 text-xs flex flex-col justify-center items-center md:order-1">
+          <span className="qrcode size-25"></span>
+          git.nickford.com/quiz
         </div>
       </div>
-      {showScoreboard && (
-        <div className="absolute top-0 left-0 w-screen h-screen p-8 bg-white/50 dark:bg-black/50 backdrop-blur overflow-auto">
-          <h2 className="mb-4 icon">Scoreboard</h2>
-          <div className="grid gap-2">
-            {scores.map((s:any, i:number) => (
-              <div key={s.id} className="flex justify-between items-stretch bg-grey-500 text-white rounded px-3 py-2">
-                <span className="tabular-nums text-right w-[3em] mr-2 text-blue-800 border border-transparent">{i+1} |</span>
-                <span className="flex-1 font-semibold border border-transparent">{s.name}</span>
-                <span className="tabular-nums text-right border rounded w-[4em] px-1">{s.correct}</span>
-              </div>
-            ))}
-            {scores.length === 0 && <div>No answers yet.</div>}
-          </div>
-        </div>
-      )}
     </div>
+    {showScoreboard && (
+      <div className="scoreboard absolute top-0 left-0 w-screen h-screen p-8 bg-grey-400/50 dark:bg-black/50 backdrop-blur overflow-auto">
+        <h2 className="mb-4 icon">Scoreboard</h2>
+        <div className="grid gap-2">
+          {scores.map((s:any, i:number) => (
+            <div key={s.id} className="flex justify-between items-stretch bg-grey-500 text-white rounded px-3 py-2">
+              <span className="tabular-nums text-right w-[3em] mr-2 text-blue-800 border border-transparent">{i+1} |</span>
+              <span className="flex-1 font-semibold border border-transparent">{s.name}</span>
+              <span className="tabular-nums text-right border rounded w-[4em] px-1">{s.correct}</span>
+            </div>
+          ))}
+          {scores.length === 0 && <div>No answers yet.</div>}
+        </div>
+      </div>
+    )}
+    </>
   );
 }
